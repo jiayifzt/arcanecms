81b3da85c304eb221c8746d3bdb4e3c8d5d72dda
Max Fierke <max@maxfierke.com>
2011-08-24 13:21:40 -0500

    +Added ThemeEngine object creation.

--- includes/master.inc.php
+++ includes/master.inc.php
@@ -38,6 +38,9 @@
     // Object for tracking and displaying error messages
     $Error = Error::getError();
 
+    // Initialize ThemeEngine (Creates a singleton ThemeEngine)
+    $themeengine = ThemeEngine::getThemeEngine();
+
     // If you need to bootstrap a first user into the database, you can run this line once
     //Auth::createNewUser('admin', 'password'); //DEBUG purposes only, obviously not a safe option.
 ?>	
